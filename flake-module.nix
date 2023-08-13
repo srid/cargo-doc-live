ac78ab2b4de27c1d2cbea73cb195e035dd7db736
Sridhar Ratnakumar <srid@srid.ca>
2023-08-12 21:30:52 -0400

    Block browser-sync from starting before docs are ready

--- flake-module.nix
+++ flake-module.nix
@@ -49,17 +49,32 @@ in
                 tui = false;
                 port = 0; # process-compose exits silently if port is in use; default is 8080.
                 settings.processes = {
-                  cargo-doc.command = builtins.toString (pkgs.writeShellScript "cargo-doc" ''
-                    run-cargo-doc() {
-                      cargo doc --document-private-items --all-features
-                      ${browser-sync} reload --port ${port}  # Trigger reload in browser
-                    }; export -f run-cargo-doc
-                    cargo watch -s run-cargo-doc
-                  '');
-                  browser-sync.command = ''
-                    ${browser-sync} start --port ${port} --ss target/doc -s target/doc \
-                      --startPath /${cfg.crateName}/
-                  '';
+                  cargo-doc = {
+                    command = builtins.toString (pkgs.writeShellScript "cargo-doc" ''
+                      run-cargo-doc() {
+                        cargo doc --document-private-items --all-features
+                        ${browser-sync} reload --port ${port}  # Trigger reload in browser
+                      }; export -f run-cargo-doc
+                      cargo watch -s run-cargo-doc
+                    '');
+                    readiness_probe = {
+                      period_seconds = 1;
+                      failure_threshold = 100000; # 'cargo doc' can take quite a while.
+                      exec.command = ''
+                        # Wait for the first 'cargo doc' to have completed.
+                        # We'll use this state to block browser-sync from starting
+                        # and opening the URL in the browser.
+                        ls target/doc/${cfg.crateName}/index.html
+                      '';
+                    };
+                  };
+                  browser-sync = {
+                    command = ''
+                      ${browser-sync} start --port ${port} --ss target/doc -s target/doc \
+                        --startPath /${cfg.crateName}/
+                    '';
+                    depends_on."cargo-doc".condition = "process_healthy";
+                  };
                 };
               };
           };
